1f5c3d538d187db2c5ca930fe315575a461e5791
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-03-02 17:01:48 +0530

    unused imports removed

--- src/main/java/dev/mksingh/springboot/Application.java
+++ src/main/java/dev/mksingh/springboot/Application.java
@@ -1,19 +1,12 @@
 package dev.mksingh.springboot;
 
-import dev.mksingh.springboot.run.Location;
-import dev.mksingh.springboot.run.Repository;
-import dev.mksingh.springboot.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.context.WebServerApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
