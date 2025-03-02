9e8740b2a0beeef8bf62ab2935961d685b7da134
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-03-02 13:05:33 +0530

    db initialized and server start log added

--- src/main/java/dev/mksingh/springboot/Application.java
+++ src/main/java/dev/mksingh/springboot/Application.java
@@ -2,12 +2,16 @@ package dev.mksingh.springboot;
 
 import dev.mksingh.springboot.run.Location;
 import dev.mksingh.springboot.run.Run;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -15,19 +19,11 @@ import java.time.temporal.ChronoUnit;
 @SpringBootApplication
 public class Application {
 
-  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-    logger.info("🚀 Application has started successfully");
-  }
-
-  @Bean
-  CommandLineRunner runner() {
-    return args -> {
-        var run = new Run(1, "First run", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5, Location.OUTDOOR);
-        logger.info("Run: {}", run);
-    };
-  }
-
+    public static void main(String[] args) {
+        var context = SpringApplication.run(Application.class, args);
+        var port = ((WebServerApplicationContext) context).getWebServer().getPort();
+        logger.info("\uD83D\uDE80 Application has started and listening at http://localhost:{}", port);
+    }
 }
