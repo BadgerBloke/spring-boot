228ce45f01f4e1a387b81b10dece6ae0400d74d1
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-03-02 13:09:04 +0530

    unused code removed

--- src/main/java/dev/mksingh/springboot/Application.java
+++ src/main/java/dev/mksingh/springboot/Application.java
@@ -1,20 +1,10 @@
 package dev.mksingh.springboot;
 
-import dev.mksingh.springboot.run.Location;
-import dev.mksingh.springboot.run.Run;
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.context.WebServerApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class Application {
